aca4af8dfded05a8c5ea002a9f4e3fa8021a2894
panopticons <thepanopticons@gmail.com>
2016-02-22 05:38:38 -0700

    Initial version of calculator.

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,63 @@
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * 
+	 * 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
